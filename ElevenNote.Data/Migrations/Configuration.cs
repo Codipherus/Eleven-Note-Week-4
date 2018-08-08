39dbc6847766052e4036e756b6a6e5c8525135e0
cjboillot <boillot.cody@gmail.com>
2018-08-08 16:00:23 -0400

    ElevenNote stuff

---
+++ ElevenNote.Data/Migrations/Configuration.cs
@@ -0,0 +1,27 @@
+namespace ElevenNote.Data.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ElevenNote.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ElevenNote.Data.ApplicationDbContext";
+        }
+
+        protected override void Seed(ElevenNote.Data.ApplicationDbContext context)
+        {
+
+            var notes = new List<Note>
+               {
+                   new Note{Title = "Eat", Content="Pizza tonight", OwnerID = Guid.NewGuid(), CreatedUtc = new DateTimeOffset() } };
+               }
+        }
+    }
+
+
